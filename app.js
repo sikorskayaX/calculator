0032cff75c6c064e3d880c8a1936098776d45013
sikorskayaX <sikorskaya02@gmail.com>
2023-12-02 02:48:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const buttons = document.querySelectorAll('.button');
+
+    const result = document.querySelector('.result');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            console.log(button.value);
+        })
+    })
+});
