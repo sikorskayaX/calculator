bc19323dcd76f4c44192e3dfc6334709ceeb96da
sikorskayaX <sikorskaya02@gmail.com>
2023-12-02 03:39:59 +0300

    Implement basic calculator functionality in app
    This commit introduces the core functionality for a simple calculator within the app.js file. Event listeners have been added to handle button clicks for numbers and operations. The logic for displaying the current equation and calculating the result upon pressing the equal sign has been implemented. Additionally, the index.html file has been updated to include a zero button, assign values to operation buttons, and display elements for the equation and result.

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 ﻿document.addEventListener('DOMContentLoaded', (event) => {
     const buttons = document.querySelectorAll('.button');
+    const innerResult = document.querySelector('.result');
+    const innerEquation = document.querySelector('.equation');
 
-    const result = document.querySelector('.result');
+    let item1 = "";
+    let item2 = "";
+    let isNum = true;
+    let sign = "";
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            console.log(button.value);
+            currentSymbol = button.value;
+            if (button.classList.contains("num"))
+            {
+                if(isNum)
+                {
+                    item1 += currentSymbol;
+                    innerEquation.innerHTML += currentSymbol;
+                }
+                else
+                {
+                    item2 += currentSymbol;
+                    innerEquation.innerHTML += currentSymbol;
+                }
+            }
+            else if (button.classList.contains("operation"))
+            {
+                isNum = false;
+                
+                {
+                    sign = currentSymbol;
+                    innerEquation.innerHTML += sign;
+                }
+            }
+            else
+            {
+                item1 = parseInt(item1);
+                item2 = parseInt(item2);
+
+                if(sign == "+"){
+                    result = item1 + item2;
+                    innerResult.innerHTML = result;
+                }
+
+                item2 = "";
+                item1 = result;
+            }
         })
     })
+
 });
